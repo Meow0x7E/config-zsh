db1605d5cff69f1d9e70cdd87c2cd7d2509546ba
Meow0x7E <133293581+Meow0x7E@users.noreply.github.com>
2025-03-16 04:26:34 +0800

    feat: 添加 alias
    一个 thefuck 的 alias
    一个 nvim 的 alias

--- before-source/10-alias.sh
+++ before-source/10-alias.sh
@@ -59,10 +59,15 @@ if { 1>&- which sudo } {
 }
 
 if { 1>&- which nvim } {
+    alias 'v'='nvim '
     alias 'vi'='nvim '
 }
 
 # 事实上我很少会误输入这个，我会在按回车前发现自己的 ls 反了，而且我更习惯使用 all
 if { 1>&- which sl } {
     alias 'sl'='sl -ce3'
+}
+
+if { 1>&- which thefuck } {
+    eval "$(thefuck --alias)"
 }
