5cf1fed1cdaba47a2429bfe2356b6fb451340497
Meow0x7E <133293581+Meow0x7E@users.noreply.github.com>
2026-02-12 21:18:19 +0800

    update

--- source/tmux-or-motd.sh
+++ source/tmux-or-motd.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/zsh
 
-# 如果有 tmux 则新建会话连接到 terminal 组
-if { 1>/dev/null which tmux } && [[ ! "$(tty)" =~ /dev/tty* && -z "$TMUX" && -z "$TERMUX_VERSION" ]] {
-  exec tmux new-session -A -c "$HOME" -s "terminal" -t "terminal"
-} else {
-  local -a fastfetch_options=(--pipe 0)
-  local cols="$(tput cols)"
-  if [[ -n "$cols" ]] && ((cols <= 90 )) {
+function _tmux() {
+  if { ! command -v tmux 1>/dev/null } {
+    return 0
+  }
+
+  if [[ "$(tty)" =~ ^/dev/tty[0-9]+$ ]] {
+    return 0
+  }
+
+  if [[ -n "$TMUX" ]] {
+    return 0
+  }
+
+  if [[ -n "$TERMUX_VERSION" ]] {
+    return 0
+  }
+
+  if [[ -n "$SSH_CONNECTION" ]] {
+    exec tmux new-session -A -c "$HOME" -s "terminal" -t "terminal"
+  } else {
+    tmux new-session -A -c "$HOME" -s "terminal" -t "terminal"
+  }
+}
+
+function _motd() {
+  typeset -a fastfetch_options=(--pipe 0)
+  typeset cols="$(tput cols)" # 获取当前终端列数
+
+  # 若终端宽度 ≤ 90 列，则将 logo 置于顶部，以节省横向空间
+  if [[ -n "$cols" ]] && (( cols <= 90 )) {
     fastfetch_options+=(--logo-position top)
   }
 
@@ -14,3 +37,8 @@ if { 1>/dev/null which tmux } && [[ ! "$(tty)" =~ /dev/tty* && -z "$TMUX" && -z 
   (fastfetch $fastfetch_options)
   unset fastfetch_options cols
 }
+
+_tmux
+_motd
+
+unfunction _tmux _motd
